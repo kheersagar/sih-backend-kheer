99027e78fa66134b3de8f980bf4524cecf24e584
kheersagar <santparja@gmail.com>
2022-08-15 18:57:29 +0530

    populated

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const ticket = require("../models/ticket");
 const getUserTicket = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await ticket.find({ userId: id });
+    const result = await ticket.find({ userId: id }).populate("monumentId");
     res.send(result);
   } catch (err) {
     res.status(500).send("some error occured");
