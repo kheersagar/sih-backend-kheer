aa86f11e9f593bb9ef2a061f21b35610f8409bb2
kheersagar parja <73149618+kheersagar@users.noreply.github.com>
2022-09-15 13:20:37 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -64,7 +64,7 @@ const payOrder = async (req, res) => {
           );
           const temp = await ticket.updateMany(
             { _id: newTicket._id },
-            { qr: qr}
+            { qr: qr.toString()}
           );
           await cart.findOneAndDelete({ userId: item.userId });
           const bookedTicket = await ticket
